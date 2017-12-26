e6ea6095cb2bfefd88f998ccdb6200bfafecf24d
junzz <34027020+zhouzj151921@users.noreply.github.com>
2017-12-26 15:13:56 +0800

    zzj

--- src/main/java/xmu/crms/view/TeacherPageController.java
+++ src/main/java/xmu/crms/view/TeacherPageController.java
@@ -1,8 +1,48 @@
 package xmu.crms.view;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value="/student")
 public class TeacherPageController {
+
+    @RequestMapping(value="/StudentBindPage")
+    public String studentBindPage() { ;return"StudentBindPage"; }
+
+    @RequestMapping(value="/StudentChooseCoursePage")
+    public String studentChooseCoursePage() { ;return"StudentChooseCoursePage"; }
+
+    @RequestMapping(value="/StudentCourseHome")
+    public String studentCourseHome() { ;return"StudentCourseHome"; }
+
+    @RequestMapping(value="/StudentCourseInformation")
+    public String studentCourseInformation() { ;return"StudentCourseInformation"; }
+
+    @RequestMapping(value="/StudentHomePage")
+    public String studentHomePage() { ;return"StudentHomePage"; }
+
+    @RequestMapping(value="/StudentModifyGroupPage")
+    public String studentModifyGroupPage() { ;return"StudentModifyGroupPage"; }
+
+    @RequestMapping(value="/StudentSeminarPage(fixed)")
+    public String studentSeminarPageFixed() { ;return"StudentSeminarPage(fixed)"; }
+
+    @RequestMapping(value="/StudentSeminarPage")
+    public String studentSeminarPage() { ;return"StudentSeminarPage"; }
+
+
+    @RequestMapping(value="/StudentViewGradePage")
+    public String studentViewGradePage() { ;return"StudentViewGradePage"; }
+
+    @RequestMapping(value="/StudentViewGroupPage")
+    public String studentViewGroupPage() { ;return"StudentViewGroupPage"; }
+
+    @RequestMapping(value="/StudentViewTopicPage(fixed)")
+    public String studentViewTopicPageFixed() { ;return"StudentViewTopicPage(fixed)"; }
+
+    @RequestMapping(value="/StudentViewTopicPage(random)")
+    public String studentViewTopicPageRandom() { ;return"StudentViewTopicPage(random)"; }
+
 
 }
