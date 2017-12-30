380757b29a1cc017591fc7227ec0e92ffc6c7b53
fishzhouz <1246022512@qq.com>
2017-12-30 14:13:49 +0800

    cb
    最新版

--- src/main/java/xmu/crms/view/MeController.java
+++ src/main/java/xmu/crms/view/MeController.java
@@ -1,49 +1,94 @@
 package xmu.crms.view;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import xmu.crms.entity.User;
+import xmu.crms.exception.UserNotFoundException;
+import xmu.crms.service.impl.UserServiceImpl;
 import xmu.crms.vo.SchoolVO;
 import xmu.crms.vo.UserVO;
 
-//@RestController
-//public class MeController {
-//
-//    @ResponseStatus(value= HttpStatus.OK)
-//    @RequestMapping(value="/me",method = RequestMethod.GET)
-//    @ResponseBody
-//    public UserVO homepage()
-//    {
-//        SchoolVO school=new SchoolVO(123,"厦门大学");
-//        UserVO user=new UserVO(3486,"XXX","XXXX","xxxxxx","xxxxxx","xxxxxx","male","xxxx","xxxxxx",school);
-//        return user;
-//    }
-//
-//    @ResponseStatus(value= HttpStatus.NO_CONTENT)
-//    @RequestMapping(value="/me",method = RequestMethod.PUT)
-//    @ResponseBody
-//    public void registerFirst(@RequestBody UserVO user)
-//    {
-//
-//    }
-//
-//    @ResponseStatus(value= HttpStatus.CREATED)
-//    @RequestMapping(value="/signin",method = RequestMethod.POST)
-//    @ResponseBody
-//    public UserVO index(@RequestBody UserDTO u)
-//    {
-//        UserVO user;
-//        if(u.getPhone().equals("123"))
-//        {
-//            user=new UserVO(3486,"李四","student","eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjaWQiOiJPQTAwMDEiLCJpYXQiOjE0ODI2NTcyODQyMjF9.TeJpy936w610Vrrm+c3+RXouCA9k1AX0Bk8qURkYkdo=");
-//        }
-//        else
-//        {
-//            user=new UserVO(3486,"张三","teacher","eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjaWQiOiJPQTAwMDEiLCJpYXQiOjE0ODI2NTcyODQyMjF9.TeJpy936w610Vrrm+c3+RXouCA9k1AX0Bk8qURkYkdo=");
-//        }
-//        System.out.print(user);
-//        return user;
-//    }
+import java.math.BigInteger;
 
+@RestController
+public class MeController {
+    @Autowired
+    private UserServiceImpl userSeviceImpl;
+    /**
+     * 获得当前用户
+     * @return User deleteNumber
+     */
+    @RequestMapping(value="/me", method=RequestMethod.GET)
+    public UserVO getUser() throws UserNotFoundException {
+        //jwt
+        BigInteger id=new BigInteger("5");
+        User u=userSeviceImpl.getUserByUserId(id);
+        UserVO u1=new UserVO();
+        u1.setId(u.getId().intValue());
+        u1.setName(u.getName());
+        if(u.getType()==1)
+            u1.setType("teacher");
+        else
+            u1.setType("student");
+        u1.setNumber(u.getNumber());
+        u1.setPhone(u.getPhone());
+        u1.setEmail(u.getEmail());
+        if(u.getGender()==1)
+            u1.setGender("female");
+        else
+            u1.setGender("male");
+        u1.setTitle(u.getTitle().toString());
+        u1.setAvatar(u.getAvatar());
+        return new UserVO();
+    }
+    SchoolVO school;
+    String jwt ;
+    /**
+     * 修改当前用户
+     * @return
+     * @throws UserNotFoundException
+     */
+    @RequestMapping(value="/me", method=RequestMethod.PUT)
+    @ResponseStatus(value=HttpStatus.NO_CONTENT)
+    public void updateUser(@RequestBody User user) throws UserNotFoundException {
+        userServiceImpl.updateUserByUserId(user.getId(), user);
+    }
+    /**
+     * 手机号登录，返回用户基本信息
+     * @param phone
+     * @param password
+     * @return
+     * @throws UserNotFoundException 用户不存在或密码不正确
+     */
+    @RequestMapping(value="/signin", method=RequestMethod.POST)
+    public User signinByPhone(@RequestBody User user) throws UserNotFoundException{
 
-//}
+        System.out.println(user);
+        return user;
+    }
+
+    /**
+     * 手机号密码注册
+     * @param user
+     * @return
+     * @throws UserNotFoundException
+     */
+    @RequestMapping(value="/registerByPhone", method=RequestMethod.POST)
+    @ResponseStatus(value=HttpStatus.NO_CONTENT)
+    public void registerByPhone(@RequestBody User user) throws UserNotFoundException{
+        loginServiceImpl. signUpPhone(user) ;
+    }
+    /**
+     * 微信登录，返回用户基本信息
+     * @param code
+     * @param state
+     * @param successUrl
+     * @param response
+     * @return User deleteUser
+     */
+    @RequestMapping(value="/signin", method=RequestMethod.GET)
+    public User signin(String code, String state, String successUrl, HttpServletResponse response){
+    }
+}
--- src/main/java/xmu/crms/vo/GroupScoreVO.java
+++ src/main/java/xmu/crms/vo/GroupScoreVO.java
@@ -1,16 +1,5 @@
 package xmu.crms.vo;
 
 public class GroupScoreVO {
- "presentationGrade": [
-    {
-        "topicId": 257,
-            "grade": 4
-    },
-    {
-        "topicId": 258,
-            "grade": 5
-    }
-  ],
-          "reportGrade": 3,
-          "grade": 4
+
 }
