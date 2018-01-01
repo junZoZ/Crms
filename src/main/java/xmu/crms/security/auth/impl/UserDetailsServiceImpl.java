0dae72a6268da52edcad9295a1cac5dc764b9ca8
ZYanXiang <34715105+ZYanXiang@users.noreply.github.com>
2018-01-02 00:27:07 +0800

    zyx

--- src/main/java/xmu/crms/security/auth/impl/UserDetailsServiceImpl.java
+++ src/main/java/xmu/crms/security/auth/impl/UserDetailsServiceImpl.java
@@ -25,18 +25,10 @@ public class UserDetailsServiceImpl implements xmu.crms.security.auth.UserDetail
     @Autowired
     private LoginService loginService;
 
-    /*
-    @Value("${wechat.mp.appid}")
-    private String appid;
 
-    @Value("${wechat.mp.secret-key}")
-    private String secret;
-    private static final String KEY_OPEN_ID = "openid";
-    private static final String KEY_ERR_CODE = "errcode";
-*/
 
     private static final String LOGIN_WITH_PHONE = "phone";
-   // private static final String LOGIN_WITH_WECHAT = "wechat";
+
 
 
     /**
@@ -50,7 +42,7 @@ public class UserDetailsServiceImpl implements xmu.crms.security.auth.UserDetail
 
     @Override
     public User loadUserByUsername(String s) throws UsernameNotFoundException {
-       // System.out.println(8);
+
         String[] m = s.split(" ");
         System.out.println("    "+ m[1] );
         User user = null;
@@ -59,17 +51,20 @@ public class UserDetailsServiceImpl implements xmu.crms.security.auth.UserDetail
             System.out.println(getUserByPhone(m[1]));
             return getUserByPhone(m[1]);
         }
-        /*else if (LOGIN_WITH_WECHAT.equals(m[0])) {
-           // return getUserByWechat(m[1]);
-        }*/
+
 
         return null;
     }
 
-    // 目前是假数据，加密算法等用数据库后删除
+    /**
+     *
+     * @param phone
+     * @return
+     * @throws UsernameNotFoundException
+     */
     private User getUserByPhone(String phone)
             throws UsernameNotFoundException {
-       // System.out.println(9);
+
 
         User users=new User();
         users.setPhone(phone);
@@ -80,47 +75,7 @@ public class UserDetailsServiceImpl implements xmu.crms.security.auth.UserDetail
         }
         return user;
     }
-    /**
-     * 获取 open id
-     * @param code
-     * @return
-     */
-    /*
-    private String getOpenId(String code) {
-        String openId = null;
-        try {
-            String urlString = String.format("https://api.weixin.qq.com/sns/jscode2session?" +
-                            "appid=%s&secret=%s&js_code=%s&grant_type=authorization_code",
-                    appid, secret, code);
-            URL url = new URL(urlString);
-            // json解析
-            ObjectMapper mapper = new ObjectMapper();
-            // 请求接口获取结果并进行json解析
-            Map<String, Object> map = mapper.readValue(url, Map.class);
-            if (map.get(KEY_OPEN_ID) != null) {
-                openId = map.get(KEY_OPEN_ID).toString();
-            } else if (map.get(KEY_ERR_CODE) != null) {
 
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        if (openId == null) {
-            throw new UsernameNotFoundException("");
-        }
-        return openId;
-    }
-    */
-/*
-    private User getUserByWechat(String code)
-            throws UsernameNotFoundException {
 
-        User user = loginService.getUserLoginByWechat(getOpenId(code));
-        if (user == null) {
-            throw new UsernameNotFoundException("");
-        }
-        return user;
-    }
-*/
 
 }
